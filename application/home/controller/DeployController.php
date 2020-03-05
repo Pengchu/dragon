867633690568a215176c9c88b5bd870ad68fb2a2
Pengchu <3207603685@qq.com>
2020-03-05 20:41:53 +0800

    增加功能页面

--- application/home/controller/DeployController.php
+++ application/home/controller/DeployController.php
@@ -5,8 +5,21 @@ use app\common\controller\BaseController;
 
 class DeployController extends BaseController
 {
+	/**
+	 * 更新列表
+	 * @return [type] [description]
+	 */
     public function index() 
     {
     	return $this->fetch();
     }	
+
+    /**
+     * 角色委托更新页面
+     * @return [type] [description]
+     */
+    public function role()
+    {
+    	return $this->fetch();
+    }
 }
